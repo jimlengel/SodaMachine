de289a8b692f721ce026df2defee250f55bc3b86
Jim Lengel <email": null>
2016-07-18 15:21:49 -0700

    commit

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda.rb'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,22 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.brand
   end
 
   def sell(soda_brand)
+    # @sodas[:]
   end
 
 end
+
+# pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+# mountain_dew = Soda.new(brand: 'Mountain Dew', price: 0.75)
+# coke_zero = Soda.new(brand: 'Coke Zero', price: 1.00)
+# second_pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+
+# p pepsi.find_soda('Pepsi')
