8bebea8eb9f6d2960c5d8fb80e1fd60aa14a4ce7
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-07-18 15:23:51 -0700

    Add methods and pass some tests

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,41 @@ class SodaMachine
   end
 
   def current_inventory_count
+    sodas.count 
   end
 
   def find_soda(soda_brand)
+    sodas.each do |item| 
+      if item.brand == soda_brand
+        return item
+      else
+        return nil
+      end
+    end
   end
 
   def sell(soda_brand)
-  end
+    sodas.each do |item|
+      if item.brand == soda_brand
+        @cash = @cash + item.price
+    end
+
+  #   if find_soda(soda_brand) == nil
+  #     return nil
+  #   end
+  #   if sodas.include?(soda_brand)
+  #     @cash += Soda.price
+  #   end
+  # end
 
 end
+
+
+
+
+
+
+
+
+
+
