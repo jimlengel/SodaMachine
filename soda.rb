b0d264cfaa8a2b0b5d24a8fcdfc2fdda7bf965aa
Sarah Spikes <sdspikes@cs.stanford.edu>
2016-07-20 11:05:30 -0700

    review session with sarah

--- soda.rb
+++ soda.rb
@@ -6,3 +6,7 @@ class Soda
     @price = args[:price]
   end
 end
+
+pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+
+p pepsi.brand
--- soda_machine.rb
+++ soda_machine.rb
@@ -13,18 +13,44 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
-    @sodas.brand
+    # @sodas.brand
+    # @sodas.each do |i|
+    #   if i.brand == soda_brand
+    #     return i
+    #   end
+    # end
+    # return nil
+
+    @sodas.find { |soda| soda.brand == soda_brand}
   end
 
   def sell(soda_brand)
-    # @sodas[:]
+    # find the soda
+    # if in machine
+      # remove soda from array
+      # add cash value of soda to machine's cash
+    # else
+    # return nil
+    soda = find_soda(soda_brand)
+    if soda
+      @sodas.delete(soda)
+      @cash += soda.price
+    else
+      return nil
+    end
   end
 
 end
 
-# pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
-# mountain_dew = Soda.new(brand: 'Mountain Dew', price: 0.75)
-# coke_zero = Soda.new(brand: 'Coke Zero', price: 1.00)
-# second_pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+mountain_dew = Soda.new(brand: 'Mountain Dew', price: 0.75)
+coke_zero = Soda.new(brand: 'Coke Zero', price: 1.00)
+second_pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
 
-# p pepsi.find_soda('Pepsi')
+machine = SodaMachine.new(
+  {
+    sodas: [pepsi, mountain_dew, coke_zero, second_pepsi]
+  }
+)
+
+p machine.find_soda('Pepsi')
