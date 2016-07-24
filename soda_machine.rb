af1ef14c460588ea162c235db669e3e61afbae5d
Jim Lengel <jimlengel@gmail.com>
2016-07-24 13:58:43 -0700

    complete all

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,26 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |i|
+      if i.brand == soda_brand
+        return i
+      end
+    end
+    return nil
   end
 
   def sell(soda_brand)
+    @sodas.each do |i|
+      if i.brand == soda_brand
+        @cash += i.price
+        @sodas.delete(i)
+      end
+    end
+  return nil
   end
 
 end
